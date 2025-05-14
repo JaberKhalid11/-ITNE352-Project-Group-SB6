32d103a2e5842a6e4f2c9fca2bebfc0e66a3e7de
JaberKhalid11 <jaberbaloch14@gmail.com>
2025-05-14 11:35:53 +0300

    added  fetch and json

--- server.py
+++ server.py
@@ -1,5 +1,17 @@
 import socket
 import threading
+import requests
+import json
+
+def fetch_flight_data(icao):
+    response = requests.get(
+        "http://api.aviationstack.com/v1/flights",
+        params={"access_key": "1a606ff09040ef8539b85c3e2ab198fb", "arr_icao": icao, "limit": 100}
+    )
+    flights = response.json().get("data", [])
+    with open("group_SB6.json", "w") as f:
+        json.dump(flights, f, indent=4)
+    return flights
 
 def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
